ece6cbc72d1ea2ccff722d7da67c6e0c20e2f771
Matematikis <68231941+Matematikis@users.noreply.github.com>
2021-09-01 20:59:26 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='karlisr_packages',
+    url='https://github.com/Matematikis/karlisr_packages',
+    author='Karlis R',
+    author_email='karlisr@dati.lv',
+    # Needed to actually package something
+    packages=['karlisr_packages'],
+    # Needed for dependencies
+    install_requires=['pandas'],
+    # *strongly* suggested for sharing
+    version='1.2.1',
+    # The license can be anything you like
+    license='MIT',
+    description='An example of a python package from pre-existing code',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
